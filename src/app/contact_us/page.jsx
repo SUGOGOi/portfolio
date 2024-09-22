38009fb32801aac9708e8dfaa85d5e31a1224841
Sumsum Gogoi <sumsumgogoi51@mail.com>
2024-09-22 19:28:17 +0530

    22/9/24

---
+++ src/app/contact_us/page.jsx
@@ -0,0 +1,9 @@
+'use client'
+
+const page = () => {
+    return (
+        <div>contactt</div>
+    )
+}
+
+export default page
---
+++ src/app/education/page.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+const page = () => {
+    return (
+
+        <div className="">
+            education
+        </div>
+
+    )
+}
+
+export default page
